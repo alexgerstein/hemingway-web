4cf6928d694e570dba0628c2ba88c0ae6bd976d2
Alex Gerstein <gerstein.alex@gmail.com>
2014-11-02 22:21:13 -0500

    Add Logging.

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 from app import views, forms
+
+if os.environ.get('HEROKU') is not None:
+	import logging
+	stream_handler = logging.StreamHandler()
+	app.logger.addHandler(stream_handler)
+	app.logger.setLevel(logging.INFO)
+	app.logger.info('WLH startup')
