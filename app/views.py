62a30d2b56d3d5a220421c0b9a282a399fa0aa2e
Alex Gerstein <gerstein.alex@gmail.com>
2014-11-04 00:04:22 -0500

    Fix indentation.

--- app/views.py
+++ app/views.py
@@ -7,17 +7,17 @@ from convert import WriteLike
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    form = InputForm()
+  form = InputForm()
 
-    if form.validate_on_submit():
-        writer = WriteLike(form.style.data)
+  if form.validate_on_submit():
+      writer = WriteLike(form.style.data)
 
-        output_text = writer.style_convert_string(form.input_text.data)
+      output_text = writer.style_convert_string(form.input_text.data)
 
-        return render_template('index.html',
-                               input_text=form.input_text.data,
-                               output_text=output_text,
-                               form=form)
+      return render_template('index.html',
+                             input_text=form.input_text.data,
+                             output_text=output_text,
+                             form=form)
 
 	return render_template('index.html',
 							form=form)
