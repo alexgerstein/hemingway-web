0afeeb81e209f796c906f2879bacbeb96de0aef2
Alex Gerstein <gerstein.alex@gmail.com>
2014-11-04 00:06:29 -0500

    Fix intendation.

--- app/views.py
+++ app/views.py
@@ -10,19 +10,19 @@ def index():
   form = InputForm()
 
   if form.validate_on_submit():
-      writer = WriteLike(form.style.data)
+    writer = WriteLike(form.style.data)
 
-      output_text = writer.style_convert_string(form.input_text.data)
+    output_text = writer.style_convert_string(form.input_text.data)
 
-      return render_template('index.html',
-                             input_text=form.input_text.data,
-                             output_text=output_text,
-                             form=form)
+    return render_template('index.html',
+                           input_text=form.input_text.data,
+                           output_text=output_text,
+                           form=form)
 
-	return render_template('index.html',
+  return render_template('index.html',
 							form=form)
 
 @app.route('/about')
 def about():
-	return render_template("about.html")
+  return render_template("about.html")
 
