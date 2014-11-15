aa01e38106f1b8573c078b24c234e9d6b24f51d6
Alex Gerstein <gerstein.alex@gmail.com>
2014-11-15 18:23:54 -0500

    Add default text for authors on swap.

--- app/static/js/translate.js
+++ app/static/js/translate.js
@@ -1,8 +1,19 @@
+// Progress Bar Constants
 var WORDS_PER_MILLISEC = 3
 var MILLISECS = 200
 
+// Default Author Text
+var HEMINGWAY = "The best way to find out if you can trust somebody is to trust them."
+var DICKENS = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair…"
+var SHAKESPEARE = "Shall I compare thee to a summer's day?\nThou art more lovely and more temperate:\nRough winds do shake the darling buds of May,\nAnd summer's lease hath all too short a date."
+var RAPPERS = "If you having girl problems I feel bad for you son. I got 99 problems but a bitch ain’t one."
+
 function getWidthPercentage(selector) {
 	return 100 * parseFloat($(selector).css('width')) / parseFloat($(selector).parent().css('width'));
+}
+
+function set_default_text(e) {
+	$("#input_text").text(window[e.target.value.toUpperCase()]);
 }
 
 function translate() {
@@ -57,4 +68,6 @@ $(document).ready(function() {
 	  .ajaxStop(function () {
 	    $loading.hide();
 	  });
+
+	$('#style').change(set_default_text);
 });
